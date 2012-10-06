b48ad5a6d380c10e0941ed57a79466f78e867ee9
PiotrN <piotrnar@gmail.com>
2012-10-06 19:14:28 +0200

    sendmany needs an account param

--- config.php
+++ config.php
@@ -10,4 +10,7 @@ define('ADMIN_PGP', "AB9EA551E262A87A13BB90591BE7B545CDF3FD0E");
 // The total amount of dividends that you are about to pay out (in BTC):
 define('TOTAL_BTC', 0.0);
 
+// Account in your wallet from which the funds shall be sent
+define('ACCOUNT', "");
+
 ?>
